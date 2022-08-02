be2df6bdaf891058646b70caa4a103768d28a9c1
Winston Limanto <wmlimanto@gmail.com>
2022-08-01 17:46:15 -0700

    add initial DayList component

---
+++ src/components/DayList.js
@@ -0,0 +1,13 @@
+import React from "react";
+import DayListItem from "./DayListItem";
+import classNames from "classnames";
+
+export default function DayList(props) {
+
+  return (
+    <ul>
+      
+    </ul>
+  )
+
+}

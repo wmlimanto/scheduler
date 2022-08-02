e83bbb04d6dcc06263ba53cb4e9ff7b493236b39
Winston Limanto <wmlimanto@gmail.com>
2022-08-01 17:33:17 -0700

    add formatSpots function to make unit tests pass

--- src/components/DayListItem.js
+++ src/components/DayListItem.js
@@ -5,6 +5,16 @@ import "components/DayListItem.scss";
 
 export default function DayListItem(props) {
 
+  const formatSpots = function(spots) {
+    if (spots === 0) {
+      return `no spots remaining`;
+    } else if (spots === 1) {
+      return `1 spot remaining`;
+    } else {
+      return `${spots} spots remaining`;
+    }
+  };
+
   const dayClass = classNames("day-list__item", {
     "day-list__item--selected": props.selected,
     "day-list__item--full": props.spots === 0
@@ -13,7 +23,7 @@ export default function DayListItem(props) {
   return (
     <li className={dayClass} onClick={() => props.setDay(props.name)}>
       <h2 className="text--regular">{props.name}</h2> 
-      <h3 className="text--light">{props.spots} spots remaining</h3>
+      <h3 className="text--light">{formatSpots(props.spots)}</h3>
     </li>
   );
 }

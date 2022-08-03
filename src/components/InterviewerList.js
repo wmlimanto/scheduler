2a86b3bb6627a484c1f7cf8ff41156d89a77b1b0
Winston Limanto <wmlimanto@gmail.com>
2022-08-02 19:39:43 -0700

    refactor props to use onChange & value for controlled list naming conventions

--- src/components/InterviewerList.js
+++ src/components/InterviewerList.js
@@ -10,8 +10,8 @@ export default function InterviewerList(props) {
       <InterviewerListItem
       key={interviewer.id}
       {...interviewer}
-      selected={interviewer.id === props.interviewer}
-      setInterviewer={() => props.setInterviewer(interviewer.id)}
+      selected={interviewer.id === props.value}
+      setInterviewer={() => props.onChange(interviewer.id)}
       />
     )
   })

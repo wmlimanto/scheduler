0899b755c987209b41b0b0844e646d050ff2c14a
Winston Limanto <wmlimanto@gmail.com>
2022-08-05 14:42:57 -0700

    combine state for day and days into single object + update props passed to DayList component

--- src/components/Application.js
+++ src/components/Application.js
@@ -47,8 +47,10 @@ const appointments = {
 
 export default function Application(props) {
 
-  const [day, setDay] = useState("Monday");
-  const [days, setDays] = useState([]);
+  const [state, setState] = useState({
+    day: "Monday",
+    days: []
+  });
 
   useEffect(() => {
     axios
@@ -79,8 +81,8 @@ export default function Application(props) {
         <hr className="sidebar__separator sidebar--centered" />
         <nav className="sidebar__menu">
         <DayList
-          days={days}
-          value={day}
+          days={state.days}
+          value={state.day}
           onChange={setDay}
         />
         </nav>

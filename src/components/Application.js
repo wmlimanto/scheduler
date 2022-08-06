e1da47d752a5af356d6f2b90c100af369943902f
Winston Limanto <wmlimanto@gmail.com>
2022-08-05 18:00:49 -0700

    fetch interviewer data + add helper getInterview to transform interview object

--- src/components/Application.js
+++ src/components/Application.js
@@ -2,7 +2,7 @@ import React, { useState, useEffect } from "react";
 import axios from "axios";
 import DayList from "./DayList";
 import Appointment from "./Appointment";
-import { getAppointmentsForDay } from "helpers/selectors";
+import { getAppointmentsForDay, getInterview } from "helpers/selectors";
 
 import "components/Application.scss";
 
@@ -12,30 +12,39 @@ export default function Application(props) {
   const [state, setState] = useState({
     day: "Monday",
     days: [],
-    appointments: {}
+    appointments: {},
+    interviewers: {}
   });
-
-  // variable to hold a list of appointments for that day
-  const dailyAppointments = getAppointmentsForDay(state, state.day);
 
   // updates the state with the new day
   const setDay = day => setState(Object.assign({}, state, {day}));
   // const setDays = (days) => setState(prev => (Object.assign({}, prev, {days})));
-
+  
   useEffect(() => {
     Promise.all([
       axios.get("/api/days"),
       axios.get("/api/appointments"),
+      axios.get("/api/interviewers")
     ]).then((all) => {
-      setState(prev => ({ ...prev, days: all[0].data, appointments: all[1].data}))
+      setState(prev => ({ 
+        ...prev, 
+        days: all[0].data, 
+        appointments: all[1].data
+      }))
     });
   })
+  
+  // variable to hold a list of appointments for that day
+  const dailyAppointments = getAppointmentsForDay(state, state.day);
 
-  const appointmentsArray = Object.values(dailyAppointments).map(appointment => {
+  const appointmentsArray = dailyAppointments.map(appointment => {
+    const interview = getInterview(state, appointment.interview);
+
     return (
       <Appointment
         key={appointment.id}
         {...appointment}
+        interview={interview}
        /> 
     )
   }) 
--- src/helpers/selectors.js
+++ src/helpers/selectors.js
@@ -1,5 +1,6 @@
 import React from 'react'
 
+// function receives two args state and day, should return an array of appointments for the given day
 export function getAppointmentsForDay(state, day) {
   const selectedDay = state.days.find(appt => appt.name === day);
   let appointments = [];
@@ -9,4 +10,17 @@ export function getAppointmentsForDay(state, day) {
   }
 
   return appointments;
-}
+};
+
+// function should return a new object containing the interview data when we pass it an object that contains the interviewer, else return null
+export function getInterview(state, interview) {
+
+  if(interview) {
+    return {
+      student: interview.student,
+      interviewer: state.interviewers[interview.interviewer]
+    }
+  }
+
+  return null;
+};

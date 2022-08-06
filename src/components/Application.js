90f0d28c56918eec3b36d4af1e8b558e92275214
Winston Limanto <wmlimanto@gmail.com>
2022-08-05 19:34:52 -0700

    add missing interviewers state to useEffect

--- src/components/Application.js
+++ src/components/Application.js
@@ -29,7 +29,8 @@ export default function Application(props) {
       setState(prev => ({ 
         ...prev, 
         days: all[0].data, 
-        appointments: all[1].data
+        appointments: all[1].data,
+        interviewers: all[2].data
       }))
     });
   })

1044ce5fd06d4c2108c15d0f210aa89de8373ce4
Winston Limanto <wmlimanto@gmail.com>
2022-08-08 17:07:24 -0700

    use useVisualMode hook to render different modes for Appointment component

--- src/components/Appointment/index.js
+++ src/components/Appointment/index.js
@@ -5,20 +5,34 @@ import "components/Appointment/styles.scss";
 import Header from "./Header";
 import Show from "./Show";
 import Empty from "./Empty";
+import useVisualMode from "hooks/useVisualMode";
+import Form from "./Form";
 
 export default function Appointment(props) {
+  const { time, interview, interviewers } = props
+  const EMPTY = "EMPTY";
+  const SHOW = "SHOW";
+  const CREATE = "CREATE";
+
+  // when props.interview contains value, pass useVisualMode the SHOW mode, if it's empty pass EMPTY mode
+  const { mode, transition, back } = useVisualMode(interview ? SHOW : EMPTY);
 
   return (
     <article className="appointment">
-      <Header time={props.time}/>
-        {props.interview ?
+      <Header time={time}/>
+      {mode === EMPTY && <Empty onAdd={() => transition(CREATE)} />}
+      {mode === SHOW && (
         <Show
-          student={props.interview.student}
-          interviewer={props.interview.interviewer}
+          student={interview.student}
+          interviewer={interview.interviewer}
         />
-        :
-        <Empty />
-        }
+      )}
+      {mode === CREATE && (
+        <Form
+          interviewers={[]}
+          onCancel={() => back(EMPTY)}
+        />
+      )}
     </article>
   );
 }
